472dd0564daad1890b70bf49db11a37c424b2bf3
pavan deepak kamble <pavandkamble010@gmail.com>
2026-03-23 19:41:09 +0530

    Refactor emotion detection to movie recommendation

--- script.js
+++ script.js
@@ -1,27 +1,59 @@
-function detectEmotion() {
+function getMovies(mood) {
 
-    const emotions = ["Happy", "Sad", "Angry", "Surprise", "Neutral"];
+    const database = {
 
-    const movies = {
-        "Happy": ["3 Idiots", "ZNMD", "Chhichhore"],
-        "Sad": ["Tamasha", "Rockstar", "Pursuit of Happyness"],
-        "Angry": ["KGF", "John Wick", "Gangs of Wasseypur"],
-        "Surprise": ["Inception", "Interstellar", "Shutter Island"],
-        "Neutral": ["Forrest Gump", "Shawshank Redemption"]
+        romantic: [
+            {title: "Titanic", img: "https://image.tmdb.org/t/p/w500/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg"},
+            {title: "The Notebook", img: "https://image.tmdb.org/t/p/w500/qom1SZSENdmHFNZBXbtJAU0WTlC.jpg"},
+            {title: "La La Land", img: "https://image.tmdb.org/t/p/w500/uDO8zWDhfWwoFdKS4fzkUJt0Rf0.jpg"},
+            {title: "Before Sunrise", img: "https://image.tmdb.org/t/p/w500/8L7l7Y0E8N8E9kXg8vY0.jpg"}
+        ],
+
+        happy: [
+            {title: "3 Idiots", img: "https://image.tmdb.org/t/p/w500/66A9MqXOyVFCssoloscw79z8Tew.jpg"},
+            {title: "Zindagi Na Milegi Dobara", img: "https://image.tmdb.org/t/p/w500/7cX6k6xW3.jpg"},
+            {title: "Chhichhore", img: "https://image.tmdb.org/t/p/w500/some.jpg"},
+            {title: "The Mask", img: "https://image.tmdb.org/t/p/w500/6fOq0Xg.jpg"}
+        ],
+
+        sad: [
+            {title: "Tamasha", img: "https://image.tmdb.org/t/p/w500/tamasha.jpg"},
+            {title: "Rockstar", img: "https://image.tmdb.org/t/p/w500/rockstar.jpg"},
+            {title: "Joker", img: "https://image.tmdb.org/t/p/w500/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg"},
+            {title: "The Pursuit of Happyness", img: "https://image.tmdb.org/t/p/w500/6dW1HLSj.jpg"}
+        ],
+
+        action: [
+            {title: "KGF", img: "https://image.tmdb.org/t/p/w500/kgf.jpg"},
+            {title: "John Wick", img: "https://image.tmdb.org/t/p/w500/fZPSd91yGE9fCcCe6OoQr6E3Bev.jpg"},
+            {title: "Avengers Endgame", img: "https://image.tmdb.org/t/p/w500/or06FN3Dka5tukK1e9sl16pB3iy.jpg"},
+            {title: "Pathaan", img: "https://image.tmdb.org/t/p/w500/pathaan.jpg"}
+        ],
+
+        thriller: [
+            {title: "Inception", img: "https://image.tmdb.org/t/p/w500/qmDpIHrmpJINaRKAfWQfftjCdyi.jpg"},
+            {title: "Interstellar", img: "https://image.tmdb.org/t/p/w500/rAiYTfKGqDCRIIqo664sY9XZIvQ.jpg"},
+            {title: "Shutter Island", img: "https://image.tmdb.org/t/p/w500/kve20tXwUZpu4GUX8l6X7Z4jmL6.jpg"},
+            {title: "Fight Club", img: "https://image.tmdb.org/t/p/w500/bptfVGEQuv6vDTIMVCHjJ9Dz8PX.jpg"}
+        ]
     };
 
-    // Random emotion (simulate AI)
-    let emotion = emotions[Math.floor(Math.random() * emotions.length)];
+    document.getElementById("title").innerText =
+        "Recommended Movies for " + mood.toUpperCase();
 
-    document.getElementById("emotion").innerText =
-        "Detected Emotion: " + emotion;
+    const container = document.getElementById("movies");
+    container.innerHTML = "";
 
-    let movieList = document.getElementById("movies");
-    movieList.innerHTML = "";
+    database[mood].forEach(movie => {
 
-    movies[emotion].forEach(movie => {
-        let li = document.createElement("li");
-        li.innerText = movie;
-        movieList.appendChild(li);
+        let card = document.createElement("div");
+        card.classList.add("movie-card");
+
+        card.innerHTML = `
+            <img src="${movie.img}" alt="">
+            <h3>${movie.title}</h3>
+        `;
+
+        container.appendChild(card);
     });
 }
